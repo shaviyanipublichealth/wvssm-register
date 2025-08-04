1461c6b473915a8ace2338570cdf89b0bd76fb9e
Shaviyani Public Health <phu.shfah@gmail.com>
2025-08-04 23:23:40 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'wvssm-cache-v4';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/icon-maskable-192.png',
-  '/icons/icon-maskable-512.png',
+  '/wvssm-register/',
+  '/wvssm-register/index.html',
+  '/wvssm-register/manifest.json',
+  '/wvssm-register/icons/icon-192.png',
+  '/wvssm-register/icons/icon-512.png',
+  '/wvssm-register/icons/icon-maskable-192.png',
+  '/wvssm-register/icons/icon-maskable-512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/css/tom-select.css',
   'https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/js/tom-select.complete.min.js'
@@ -41,7 +41,7 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(cached => {
       return cached || fetch(event.request).catch(() => {
         if (event.request.mode === 'navigate') {
-          return caches.match('/index.html');
+          return caches.match('/wvssm-register/index.html');
         }
       });
     })
